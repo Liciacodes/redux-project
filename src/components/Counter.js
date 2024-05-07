c815b8eaf2ea25137e94ecfd2fb8a78a4b3538b9
Liciacodes <udosenfelicia@gmail.com>
2024-05-07 11:41:15 +0100

    implemented login and logout authentication using redux toolkit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,36 @@
-import classes from './Counter.module.css';
+import { useSelector, useDispatch } from "react-redux";
+
+import classes from "./Counter.module.css";
+import { counterActions } from "../store/counter";
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter.counter);
+  const show = useSelector((state) => state.counter.showCounter);
+  const incrementhandler = () => {
+    dispatch(counterActions.increment());
+  };
+
+  const incrementhandlerby5 = () => {
+    dispatch(counterActions.increase(10));
+  };
+
+  const decrementhandler = () => {
+    dispatch(counterActions.decrement());
+  };
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter());
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementhandler}>Increment</button>
+        <button onClick={incrementhandlerby5}>Increase by 10</button>
+        <button onClick={decrementhandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
